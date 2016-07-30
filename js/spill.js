c32909b2cb9d6356b29778b1a596db1a483327ab
slowglass <Christopher.James.Davies@gmail.com>
2016-07-30 22:53:54 +0100

    Updated Spill
    Added config setters and fixed syntax and obvious bugs

--- js/spill.js
+++ js/spill.js
@@ -1,51 +1,72 @@
 var Spill = function() {
   var self=this;
-  this.tilt=0;
-  this.lowerBound=15;
-  this.upperBound=30;
-  this.maxDelay=2000;
-  this.status = "OFF";
   this.sound=new Howl({ src: ["music/ting.wav"], volume: 1.0});
   this.sound.onend(function() { self.setStatusDelay(); }
 }
 
-Spill.prototype.setStatusOff() {
+Spill.prototype = {
+  tilt: 0,
+  lowerBound: 15,
+  upperBound: 30,
+  maxDelay: 2000,
+  status: "OFF"
+}
+
+Spill.prototype.setStatusOff = function() {
   this.status = "OFF";
   this.sound.stop();
   clearTimeout(this.delayid);
 }
 
-Spill.prototype.setStatusOK() {
+Spill.prototype.setStatusOK = function() {
   this.status = "OK";
 }
 
-Spill.prototype.setStatusPlay() {
+Spill.prototype.setStatusPlay = function() {
   this.status = "PLAY";
   this.sound.play();
 }
 
-Spill.prototype.setStatusDelay() {
+Spill.prototype.setStatusDelay = function() {
   var self = this;
   this.status = "DELAY";
   this.delayid = setTimeout(funcion() { self.replay(); }, this.delay());
 }
 
-Spill.prototype.replay() {
-  if (this.tilt < this.lowerBound)
-    ths.setStatusOK();
+Spill.prototype.replay = function() {
+  if (this.tilt >= this.lowerBound)
+    this.setStatusPlay();
   else
-    setStatusPlay();
+    this.setStatusOK();
 }
 
-Spill.prototype.delay()
+Spill.prototype.delay = function()
 {
   var v=Math.min(Math.max(this.lowerBound,this.tilt),this.upperBound);
   var d=this.maxDelay*(v-this.lowerBound)/(this.upperBound-this.lowerBound);
   return d;
 }
 
-Spill.prototype.setTilt(t)
+Spill.prototype.setTilt = function(t)
 {
   this.tilt=t;
+  if (t>=this.lowerBound && this.status=="OK")
+    this.setStatusPlay();
 }
 
+Spill.prototype.setRange = function(mn,mx)
+{
+  this.lowerBound=mn;
+  this.upperBound=mx;
+}
+
+
+Spill.prototype.setMaxDelay = function(d)
+{
+  this.maxDelay=d;
+}
+
+Spill.prototype.setMusic = function(url)
+{
+  this.sound=new Howl({ src: [url], volume: 1.0});
+}
