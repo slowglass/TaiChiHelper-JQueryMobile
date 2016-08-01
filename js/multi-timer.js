a6343f6c9dae18c13d541e787275aae111fbe023
Chris Davies <Christopher.James.Davies@gmail.com>
2016-08-01 07:51:43 +0100

    Refactored multi-timer to add 3rd callback

--- js/multi-timer.js
+++ js/multi-timer.js
@@ -1,10 +1,11 @@
 var MultiTimer = function() {
+  this.updateDisplay();
 }
 
 MultiTimer.prototype = {
   timer: {  sec: 30, min: 0, iter: 1 },
-  sec: 0, min: 0, iter: 0,
-  timerid = null;
+  sec: 0, min: 0, iter: -1,
+  timerid: null,
   
   start: function() {
     var self=this;
@@ -12,48 +13,56 @@ MultiTimer.prototype = {
     this.sec=10;
     this.min=0;
     this.iter=this.timer.iter;
+    this.updateDisplay();
   },
   
   stop: function() {
-    if (timerid!=null) clearInterval(this.timerid=null;);
+    if (this.timerid!=null) clearInterval(this.timerid);
     this.timerid=null;
     this.iter=-1;
-    updateDisplay();
-  }
+    this.updateDisplay();
+  },
   
   tick: function() {
     var callback=null;
-    this.timer.sec--;
-    if (this.sec<0) { this.sec= 59; this.min--; callback=this.minRollover; }
-    if (this.min<0) { this.sec= this.timer.sec; this.min=this.timer.min; this.iter--; callback=this.iterRollover; }
-    if (this.iter<0) this.stop();
+    this.sec--;
+    if (this.sec<0) { this.sec= 59; this.min--; callback=this.minRolloverCB; }
+    if (this.min<0) { this.sec= this.timer.sec; this.min=this.timer.min; this.iter--; callback=this.iterRolloverCB; }
     this.updateDisplay();
     if (callback != null) callback();
+    if (this.iter<0)
+    {
+      this.stop();
+      if (this.finishedCB != null) this.finishedCB();
+    }
   },
   
   updateDisplay: function() { 
-    var t;
-    if (this.iter<0)
-      t="";
-    if (this.iter == this.timer.iter)
-      t=this.getNumber(this.sec);
-    else
-      t = this.iter + ":" + this.getNumber(this.min) ":" + this.getNumber(this.sec) ;
-      
-    $("#timer").text(t);
+    var cd, it;
+    if (this.iter<0) { it = this.timer.iter; cd = this.getNumber(this.timer.min) + ":" + this.getNumber(this.timer.sec); }
+    else if (this.iter == this.timer.iter) { it = "Starting"; cd = this.getNumber(this.sec); }
+    else { it = this.iter+1; cd = this.getNumber(this.min) + ":" + this.getNumber(this.sec); }
+    $("#standing-stake .iter-count").text(it);
+    $("#standing-stake .countdown").text(cd);
   },
   
   getNumber: function(n) {
     if (n>9) return n.toString();
     else return '0'+n.toString();
   },
+
+
+  on: function(n, cb) {
+    if (n=="min-rollover") this.minRolloverCB=cb;
+    if (n=="iter-rollover") this.iterRolloverCB=cb;
+    if (n=="finished") this.finishedCB=cb;
+    return this;
+  },
   
   getIterations: function(i) { return this.timer.iter; },
-  getDuration: function(m,s) { return this.timer.se + this.timer.min*60; }
-  getDuration: function(m,s) { this.timer.sec=s; this.timer.min=m; }
+  getDuration: function(m,s) { return this.timer.se + this.timer.min*60; },
+  getDuration: function(m,s) { this.timer.sec=s; this.timer.min=m; },
   setIterations: function(i) { this.timer.iter=i; },
   setDuration: function(s) { this.timer.sec=s%60; this.timer.min=s/60; }
-  setMinRollover: function(cb) { this.minRollover=cb; }
-  setIterRollover: function(cb) { this.iterRollover=cb; }
 }
   
