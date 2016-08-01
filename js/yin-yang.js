6697d8b3bdd0a0a27459cce34ed39a727307db57
Chris Davies <Christopher.James.Davies@gmail.com>
2016-08-01 07:50:03 +0100

    Refactored Yin-Yang to spin both ways

--- js/yin-yang.js
+++ js/yin-yang.js
@@ -2,38 +2,81 @@
 
 var YinYang = function(loc, fg, bg) {
   this._init_();
-  $(loc).append('<svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        height="256" width="256" 
-        viewBox="-8 -8 16 16"
-        class="yin-yang">
-					<g><circle r="7" fill="#808080" filter="url(#f)"/>
-						<path d="m0-7a3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 0 0 7 7 7 0 1 1 0-14z" fill="'+fg+'"/>
-						<path d="m0-7a7 7 0 1 1 0 14 3.5 3.5 0 1 1 0-7 3.5 3.5 0 1 0 0-7z" fill="'+bg+'"/>
-						<circle cy="-3.5" r="1.1" fill="'+bg+'"/><circle cy="3.5" r="1.1" fill="'+fg+'"/>
-					</g></svg>');
-		this.$svg = $(loc + " yin-yang");
+  $(loc).append('<div style="margin:0; height: 400px;"></div>');
+  $pdiv=$(loc).find("div");
+  $pdiv.append('<div style="margin:auto; width: 256px; height: 256px;"></div>');
+  $div=$pdiv.find("div");
+  $div.append('<div class="iter-count" style="margin:0; height: 1.1em; font-size: 4em; width: 100%; text-align: center"></div>');
+  $div.append('<div class="yin-yang">' +
+      '<svg ' +
+      'xmlns="http://www.w3.org/2000/svg" ' +
+      'height="256" width="256" ' +
+     'viewBox="-8 -8 16 16" ' +
+      '> ' +
+		   '<g><circle r="7" fill="#808080" filter="url(#f)"/> ' +
+		      '<path d="m0-7a3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 0 0 7 7 7 0 1 1 0-14z" fill="'+fg+'"/> ' +
+				  '<path d="m0-7a7 7 0 1 1 0 14 3.5 3.5 0 1 1 0-7 3.5 3.5 0 1 0 0-7z" fill="'+bg+'"/> ' +
+				  '<circle cy="-3.5" r="1.1" fill="'+bg+'"/><circle cy="3.5" r="1.1" fill="'+fg+'"/>' +
+		  	'</g></svg></div>');
+  $div.append('<div class="countdown" style="margin:0; height: 1.1em; font-size: 4em; width: 100%; text-align: center"></div>');
+  this.yin = $div.find(".yin-yang");
+  this.yang = $div.find(".yin-yang svg");
+  this.yin.css({
+    'animation-name': 'yin-yang-anticlockwise-spin',
+    'animation-duration': '4s',
+    'animation-timing-function': 'linear',  
+    'animation-iteration-count': 'infinite',
+    "animation-play-state": "paused",
+    'width': '256px',
+    'height': '256px',
+    'margin': '0',
+    'padding': '0'
+  });
+  this.yang.css({
+    'animation-name': 'yin-yang-clockwise-spin',
+    'animation-duration': '4s',
+    'animation-timing-function': 'linear',  
+    'animation-iteration-count': 'infinite',
+    "animation-play-state": "paused",
+    'width': '256px',
+    'height': '256px',
+    'margin': '0',
+    'padding': '0'
+  });
+  this.spinDir=0;
 }
 
 YinYang.prototype = {
   _init_: function() {
     if (YinYang.prototype.haskeyframes == undefined) {
-      $('body').append('<style> @keyframes yin-yang-spin {  100% { transform: rotate(360deg); } }</style>');
+      $('body').append('<style>' +
+          '@keyframes yin-yang-clockwise-spin     {  100% { transform: rotate( 360deg); } } ' +
+          '@keyframes yin-yang-anticlockwise-spin {  100% { transform: rotate(-360deg); } } ' +
+        '</style>');
       YinYang.prototype.haskeyframes = true;
     }
-  pause: function() {
-    this.$svg
-      .css('animation', 'yin-yang-spin 2s infinite linear');
-      .css("webkitAnimationPlayState", "running");
-  }
-  
-  spin: function() {
-    this.$svg.css("webkitAnimationPlayState", "paused");
-  }
-  
+  },
+
   stop: function() {
-    this.$svg
-      .css('animation', '');
-      .css("webkitAnimationPlayState", "");
+    this.yin .css("animation-play-state", "paused");
+    this.yang.css("animation-play-state", "paused");
+  },
+  
+  spinClockwise: function() {
+    this.spinDir=1;
+    this.yin .css("animation-play-state", "running");
+    this.yang.css({"animation-play-state": "paused"});
+  },
+
+  spinAnticlockwise: function() {
+    this.spinDir= -1;
+    this.yin .css("animation-play-state", "paused");
+    this.yang.css({"animation-play-state": "running"});
+  },
+
+  spin: function() { this.spinAnticlockwise(); },
+  toggle: function() {
+    if (this.spinDir == -1) this.spinClockwise();
+    else this.spinAnticlockwise();
   }
 }
