22cb8a0d8c62c4e03e71ff018e1e027159cee316
Chris Davies <cdavies@itrsgroup.com>
2016-08-02 15:06:10 +0100

    Corrected config setting
      They now persist and pass on the the relevant page

--- js/config.js
+++ js/config.js
@@ -1,6 +1,26 @@
 var Config = function(ss, sd) {
-	this.ss.instance = ss;
-	this.sd.instance = sd;
+	this.ss = ss;
+	this.sd = sd;
+	if (typeof(Storage) === "undefined") 
+		this.ls = {};
+	else
+		this.ls = localStorage;
+	if (this.ls.ss_dur == undefined) this.ls.ss_dur=3;
+	if (this.ls.ss_iter == undefined) this.ls.ss_iter=2;
+	if (this.ls.sd_dur == undefined) this.ls.sd_dur=0;
+	if (this.ls.sd_mx == undefined) this.ls.sd_mx=15;
+	if (this.ls.sd_mn == undefined) this.ls.sd_mn=15;
+
+
+	$("#ss-dur").val(this.ls.ss_dur).selectmenu("refresh", true).on("change", function() { c.update(); });
+	$("#ss-iter").val(this.ls.ss_iter).slider("refresh").on("change", function() { c.update(); });
+
+	$("#sd-dur").val(this.ls.sd_dur).slider("refresh").on("change", function() { c.update(); });
+	$("#sd-mn").val(this.ls.sd_mx).slider("refresh").on("change", function() { c.update(); });
+	$("#sd-mx").val(this.ls.sd_mx).slider("refresh").on("change", function() { c.update(); });
+
+	this.update();
+
 }
 
 Config.prototype = {
@@ -8,17 +28,21 @@ Config.prototype = {
 	sd: {},
 
 	update: function() {
-		this.ss.dur=$("#ss-dur").val();
-		this.ss.iter=$("#ss-iter").val();
+		this.ls.ss_dur=$("#ss-dur").val();
+		this.ls.ss_iter=$("#ss-iter").val();
 
-		this.sd.dur=$("#sd-dur").val();
-		this.sd.mx=$("#sd-mx").val();
-		this.sd.mn=$("#sd-mn").val();
+		this.ls.sd_dur=$("#sd-dur").val();
+		this.ls.sd_mx=$("#sd-mx").val();
+		this.ls.sd_mn=$("#sd-mn").val();
 
-		this.ss.instance.setIterations(this.ss.iter);
-		this.ss.instance.setDuration(this.ss.dur*60);
+		this.ss.setIterations(this.ls.ss_iter);
+		this.ss.setDuration(this.ls.ss_dur*60);
 
-		this.sd.spill.setMaxDelay(this.sd.dur);
-		this.sd.spill.setRange(this.sd.mn, this.sd.mx);
+		this.sd.spill.setMaxDelay(this.ls.sd_dur);
+		this.sd.spill.setRange(this.ls.sd_mn, this.ls.sd_mx);
+
+
+		console.log("SS:"+this.ls.ss_dur+", "+this.ls.ss_iter);
+		console.log("SD:"+this.ls.sd_dur+", ["+this.ls.sd_mn +", "+this.ls.sd_mx+"]");
 	}
 }
